f0a7d05c124044ea6af679ea98b85d7210c3b78b
Tauqeer-Mindvalley <164833402+Tauqeer-Mindvalley@users.noreply.github.com>
2025-04-18 09:37:55 +0700

    [Chore] Added Constants.swift

---
+++ DomainSearch/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constangs.swift
+//  DomainSearch
+//
+//  Created by Tauqeer on 18/04/25.
+//
+
+struct ApiConstants {
+    static let baseUrl = "https://api.domainsdb.info/v1"
+}
+
+struct AppConstants {
+    static let searchStartMinLength: Int = 3
+}
